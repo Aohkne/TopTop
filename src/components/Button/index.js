6c3eaa75f98c5103c20ad86fe097105b7d7d6c21
KHOA <aohkne@gmail.com>
2024-09-21 23:29:33 +0700

    twelefth commit
    Using PropTypes library

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,20 +1,36 @@
-import classNames from "classnames/bind";
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Button({ to, href, primary = false, outline = false, text = false, rounded = false, disable = false, small = false, large = false, children, className, leftIcon, rightIcon, onClick, ...passProps }) {
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disable = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
     let Comp = 'button';
     const props = {
         onClick,
         ...passProps,
     };
 
-
     //Remove event listener when btn is disable
     if (disable) {
-        Object.keys(props).forEach(key => {
+        Object.keys(props).forEach((key) => {
             if (key.startsWith('on') && typeof props[key] === 'function') {
                 delete props[key];
             }
@@ -48,5 +64,22 @@ function Button({ to, href, primary = false, outline = false, text = false, roun
         </Comp>
     );
 }
+
+Button.protoTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    rounded: PropTypes.bool,
+    disable: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
 
 export default Button;
--- src/components/Popper/Menu/Header.js
+++ src/components/Popper/Menu/Header.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
@@ -17,4 +18,9 @@ function Header({ title, onBack }) {
     );
 }
 
-export default Header;  
+Header.protoTypes = {
+    title: PropTypes.string.isRequired,
+    onBack: PropTypes.func.isRequired,
+};
+
+export default Header;
